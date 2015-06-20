59daa4b656ff06c13fac37ede4b77746103c488c
kiwipion <nige.henshaw@gmail.com>
2015-06-20 15:16:33 +0900

    Initial commit

---
+++ app/src/main/java/nigelhenshaw/com/cameraintenttutorial/CamaraIntentActivity.java
@@ -0,0 +1,120 @@
+package nigelhenshaw.com.cameraintenttutorial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class CamaraIntentActivity extends Activity {
+
+    private static final int ACTIVITY_START_CAMERA_APP = 0;
+    private ImageView mPhotoCapturedImageView;
+    private String mImageFileLocation = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camara_intent);
+
+        mPhotoCapturedImageView = (ImageView) findViewById(R.id.capturePhotoImageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_camara_intent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void takePhoto(View view) {
+        Intent callCameraApplicationIntent = new Intent();
+        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = null;
+        try {
+           photoFile = createImageFile();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+
+        startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
+            // Toast.makeText(this, "Picture taken successfully", Toast.LENGTH_SHORT).show();
+            // Bundle extras = data.getExtras();
+            // Bitmap photoCapturedBitmap = (Bitmap) extras.get("data");
+            // mPhotoCapturedImageView.setImageBitmap(photoCapturedBitmap);
+            // Bitmap photoCapturedBitmap = BitmapFactory.decodeFile(mImageFileLocation);
+            // mPhotoCapturedImageView.setImageBitmap(photoCapturedBitmap);
+            setReducedImageSize();
+
+        }
+    }
+
+    File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMAGE_" + timeStamp + "_";
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(imageFileName,".jpg", storageDirectory);
+        mImageFileLocation = image.getAbsolutePath();
+
+        return image;
+
+    }
+
+    void setReducedImageSize() {
+        int targetImageViewWidth = mPhotoCapturedImageView.getWidth();
+        int targetImageViewHeight = mPhotoCapturedImageView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+        int cameraImageWidth = bmOptions.outWidth;
+        int cameraImageHeight = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(cameraImageWidth/targetImageViewWidth, cameraImageHeight/targetImageViewHeight);
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inJustDecodeBounds = false;
+
+        Bitmap photoReducedSizeBitmp = BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+        mPhotoCapturedImageView.setImageBitmap(photoReducedSizeBitmp);
+
+
+    }
+}
